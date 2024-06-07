4caaefbecf898cba0f9b549976a70dd8f0e03438
Benedikt Seeger <benedikt.seeger@web.de>
2024-06-07 09:20:56 +0200

    Added validation, more basic images

--- src/main.py
+++ src/main.py
@@ -2,20 +2,20 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 import cv2
 
-model = YOLO()
+model = YOLO(r"C:\Users\Benedikt Seeger\PycharmProjects\BV2_2\BV2-project\runs\detect\train13\weights\best.pt")
 
 if __name__ == '__main__':
 
-    model.train(data=r"C:\Users\Benedikt Seeger\PycharmProjects\BV2-project\data\train.yaml",
-                epochs=400,
-                patience=50,
-                device=0,
-                fliplr=0.0,
-                flipud=0.0,
-                copy_paste=0.5,
-                perspective=0.0001,
-                batch=-1
-                )
-    results = model.predict(source=r"C:\Users\Benedikt Seeger\PycharmProjects\BV2-project\data\test_images\Fotolia_170009629_S.jpg", show=True)
+    # #model.train(data=r"C:\Users\Benedikt Seeger\PycharmProjects\BV2-project\data\train.yaml",
+    #             epochs=400,
+    #             patience=50,
+    #             device=0,
+    #             fliplr=0.0,
+    #             flipud=0.0,
+    #             copy_paste=0.5,
+    #             perspective=0.0001,
+    #             batch=-1
+    #             )
+    results = model.predict(source=r"C:\Users\Benedikt Seeger\Documents\#Ben\05-Studium\8. Semester\04-Bildverarbeitung 2\Projekt\dataset\vidoes\Heilbronn Nacht.mp4", show=True)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
